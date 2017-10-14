21776684b07dabf2b76694b31a53ff0697dd4bc1
h2k-ckhong <ckhong@h2kresearch.com>
2017-10-14 16:19:06 +0900

    Tutorial skip function added via alert dialog

--- app/src/main/java/com/h2kresearch/iepread/TutorialActivity.java
+++ app/src/main/java/com/h2kresearch/iepread/TutorialActivity.java
@@ -1,10 +1,12 @@
 package com.h2kresearch.iepread;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.os.StrictMode.ThreadPolicy;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -12,8 +14,10 @@ import android.widget.ImageView;
 public class TutorialActivity extends AppCompatActivity {
 
   ImageView imageView;
+  MediaPlayer mediaPlayer;
+  int resourceNumber;
 
-  @Override
+    @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_tutorial);
@@ -39,6 +43,25 @@ public class TutorialActivity extends AppCompatActivity {
       AnimationDrawable animation = (AnimationDrawable) imageView.getDrawable();
       animation.setOneShot(true);
       animation.start();
+
+      AlertDialog.Builder alert_confirm = new AlertDialog.Builder(TutorialActivity.this);
+      alert_confirm.setMessage("튜토리얼을 계속하시겠습니까?").setCancelable(false).setPositiveButton("네",
+          new DialogInterface.OnClickListener() {
+          @Override
+              public void onClick(DialogInterface dialog, int which) {
+                  // 'YES'
+          }
+        }).setNegativeButton("아니오",
+          new DialogInterface.OnClickListener() {
+          @Override
+              public void onClick(DialogInterface dialog, int which) {
+                  // 'No'
+                  finish();
+                  killMediaPlayer();
+              }
+        });
+      AlertDialog alert = alert_confirm.create();
+      alert.show();
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -52,13 +75,28 @@ public class TutorialActivity extends AppCompatActivity {
       AnimationDrawable animation = (AnimationDrawable) imageView.getDrawable();
       animation.setOneShot(true);
       animation.start();
+        AlertDialog.Builder alert_confirm = new AlertDialog.Builder(TutorialActivity.this);
+        alert_confirm.setMessage("튜토리얼을 계속하시겠습니까?").setCancelable(false).setPositiveButton("네",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 'YES'
+                    }
+                }).setNegativeButton("아니오",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 'No'
+                        finish();
+                        killMediaPlayer();
+                    }
+                });
+        AlertDialog alert = alert_confirm.create();
+        alert.show();
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
-
-  MediaPlayer mediaPlayer;
-  int resourceNumber;
 
   private void killMediaPlayer() {
     if (mediaPlayer != null) {
